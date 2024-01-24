f1bb0b80cfe9cd89d8834e4b813c9001f19af761
Bardin08 <vbardin810@gmail.com>
2024-01-24 02:18:28 +0200

    Convert top-level statement to Program class

--- backend/Fusion/Fusion.RestApi/Program.cs
+++ backend/Fusion/Fusion.RestApi/Program.cs
@@ -2,38 +2,44 @@ using System.Diagnostics.CodeAnalysis;
 using Fusion.RestApi.Extensions;
 using Fusion.RestApi.Routing;
 
-var builder = WebApplication.CreateBuilder(args);
-AddFusionServices(builder);
-
-var app = builder.Build();
-ConfigureMiddlewares(app);
-
-app.Run();
-return;
-
-
-void AddFusionServices(IHostApplicationBuilder appBuilder)
-{
-    var services = appBuilder.Services;
-
-    services
-        .AddProblemDetails()
-        .AddHealthChecks();
-
-    services
-        .AddFusionApiVersioning()
-        .AddFusionSwaggerGen()
-        .AddFusionOptions(appBuilder.Configuration);
-}
-
-void ConfigureMiddlewares(WebApplication appBuilder)
-{
-    appBuilder.MapFusionRouting();
-    appBuilder.UseFusionSwaggerUi();
-}
+namespace Fusion.RestApi;
 
 /// <summary>
 /// Application entry point
 /// </summary>
 [ExcludeFromCodeCoverage]
-public static partial class Program;
+public static class Program
+{
+    internal static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        AddFusionServices(builder);
+
+        var app = builder.Build();
+        ConfigureMiddlewares(app);
+
+        app.Run();
+        return;
+
+
+        void AddFusionServices(IHostApplicationBuilder appBuilder)
+        {
+            var services = appBuilder.Services;
+
+            services
+                .AddProblemDetails()
+                .AddHealthChecks();
+
+            services
+                .AddFusionApiVersioning()
+                .AddFusionSwaggerGen()
+                .AddFusionOptions(appBuilder.Configuration);
+        }
+
+        void ConfigureMiddlewares(WebApplication appBuilder)
+        {
+            appBuilder.MapFusionRouting();
+            appBuilder.UseFusionSwaggerUi();
+        }
+    }
+}

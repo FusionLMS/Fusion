1ac069e4bf5d12510600e37bb37eccb13fde86e7
Bardin08 <vbardin810@gmail.com>
2024-02-01 21:55:54 +0200

    Add profile service implementation

---
+++ backend/Fusion/Fusion.Core/DependencyInjectionExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Fusion.Core.Profile;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Fusion.Core;
+
+public static class DependencyInjectionExtensions
+{
+    public static IServiceCollection AddFusionCore(this IServiceCollection services)
+    {
+        services.AddScoped<IProfileService, ProfileService>();
+
+        return services;
+    }
+}
---
+++ backend/Fusion/Fusion.Core/Profile/ProfileService.cs
@@ -0,0 +1,39 @@
+﻿using Fusion.Infrastructure.Database.Abstractions;
+using Fusion.Infrastructure.Profile;
+
+namespace Fusion.Core.Profile;
+
+public interface IProfileService
+{
+    Task<ProfileDto> Create(ProfileDto profileDto);
+}
+
+public class ProfileService(
+    IProfileRepository profileRepository) : IProfileService
+{
+    public async Task<ProfileDto> Create(ProfileDto profileDto)
+    {
+        ArgumentNullException.ThrowIfNull(profileDto);
+
+        var spec = ProfileSpecs.ByFirstName(profileDto.FirstName) &
+                   ProfileSpecs.ByLastName(profileDto.LastName);
+        var isDuplicate = await profileRepository.ExistsBySpecification(spec);
+        if (isDuplicate)
+        {
+            // ErrorOr...
+            return null!;
+        }
+
+        var entity = new ProfileEntity
+        {
+            FirstName = profileDto.FirstName,
+            LastName = profileDto.LastName,
+            Email = profileDto.Email,
+        };
+
+        entity = await profileRepository.Create(entity);
+
+        profileDto = profileDto with { Id = entity.Id };
+        return profileDto;
+    }
+}

84b6e4fa33bd0e785294ba86a13e40d4c314f6bb
Bardin08 <vbardin810@gmail.com>
2024-02-02 12:52:34 +0200

    Remove unused profiles specs

--- backend/Fusion/Fusion.Infrastructure/Profile/ProfileSpecs.cs
+++ backend/Fusion/Fusion.Infrastructure/Profile/ProfileSpecs.cs
@@ -6,20 +6,6 @@ namespace Fusion.Infrastructure.Profile;
 public static class ProfileSpecs
 {
     public static ByEmailSpecification ByEmail(string email) => new(email);
-    public static ByFirstNameSpecification ByFirstName(string name) => new(name);
-    public static ByLastNameSpecification ByLastName(string name) => new(name);
-}
-
-public class ByFirstNameSpecification(string firstName) : Specification<ProfileEntity>
-{
-    public override Expression<Func<ProfileEntity, bool>> ToExpression()
-        => x => x.FirstName.Equals(firstName);
-}
-
-public class ByLastNameSpecification(string lastName) : Specification<ProfileEntity>
-{
-    public override Expression<Func<ProfileEntity, bool>> ToExpression()
-        => x => x.LastName.Equals(lastName);
 }
 
 public class ByEmailSpecification(string email) : Specification<ProfileEntity>

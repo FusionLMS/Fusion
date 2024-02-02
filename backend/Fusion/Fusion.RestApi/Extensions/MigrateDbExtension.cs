0932fdce8acd732ad4397f9bcb1edbd661ed4a53
dexon03 <dima0958757803@gmail.com>
2024-02-02 13:39:28 +0200

    added auto migrate

---
+++ backend/Fusion/Fusion.RestApi/Extensions/MigrateDbExtension.cs
@@ -0,0 +1,21 @@
+﻿using Fusion.Infrastructure.Database;
+using Microsoft.EntityFrameworkCore;
+
+namespace Fusion.RestApi.Extensions;
+
+public static class MigrateDbExtension
+{
+   /// <summary>
+   /// Extension method to apply migrations to the database on application startup
+   /// </summary>
+   /// <param name="app"></param>
+   /// <returns></returns>
+   public static void ApplyMigrations(this IApplicationBuilder app)
+   {
+      using IServiceScope serviceScope = app.ApplicationServices.CreateScope();
+
+      using ApplicationDbContext dbContext = serviceScope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+      dbContext.Database.Migrate();
+   }
+}

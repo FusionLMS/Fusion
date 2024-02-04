8e98da327fd25ad7aca082067d9ebcf283891549
Bardin08 <vbardin810@gmail.com>
2024-02-04 23:34:56 +0200

    Add Auth0 clients to DI

--- backend/Fusion/Fusion.Infrastructure/DependencyInjectionExtension.cs
+++ backend/Fusion/Fusion.Infrastructure/DependencyInjectionExtension.cs
@@ -1,6 +1,9 @@
 ﻿using System.Diagnostics.CodeAnalysis;
+using Auth0Net.DependencyInjection;
+using Fusion.Infrastructure.Auth.Options;
 using Fusion.Infrastructure.Database.Abstractions;
 using Fusion.Infrastructure.Database.Repositories;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Fusion.Infrastructure;
@@ -8,10 +11,35 @@ namespace Fusion.Infrastructure;
 [ExcludeFromCodeCoverage]
 public static class DependencyInjectionExtension
 {
-    public static IServiceCollection AddFusionInfrastructure(this IServiceCollection services)
+    public static IServiceCollection AddFusionInfrastructure(
+        this IServiceCollection services, IConfiguration configuration)
     {
         services.AddScoped<IProfileRepository, ProfileRepository>();
 
+        services.AddAuth0Services(configuration);
+
         return services;
+    }
+
+    private static void AddAuth0Services(
+        this IServiceCollection services, IConfiguration configuration)
+    {
+        const string auth0SectionName = "Fusion:Auth0";
+
+        services.Configure<Auth0Options>(
+            configuration.GetSection(auth0SectionName));
+
+        services
+            .AddAuth0AuthenticationClient(opt =>
+            {
+                opt.Domain = configuration[auth0SectionName + ":Domain"]!;
+                opt.ClientId = configuration[auth0SectionName + ":ClientId"];
+                opt.ClientSecret = configuration[auth0SectionName + ":ClientSecret"];
+            });
+
+        services
+            .AddAuth0ManagementClient()
+            .AddManagementAccessToken(opt =>
+                opt.Audience = configuration[auth0SectionName + ":Audience"]);
     }
 }

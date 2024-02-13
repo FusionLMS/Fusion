55e1d5685cbe12ebd9340ee754ce363f0e87e9fd
Bardin08 <vbardin810@gmail.com>
2024-02-13 23:47:38 +0200

    Add AuthService tests

---
+++ backend/Fusion/Fusion.Tests/Core/Auth/AuthServiceTests.cs
@@ -0,0 +1,129 @@
+﻿using Auth0.ManagementApi;
+using Auth0.ManagementApi.Models;
+using Auth0.ManagementApi.Paging;
+using FluentAssertions;
+using Fusion.Core.Auth;
+using Fusion.Infrastructure.Database.Abstractions;
+using Fusion.Infrastructure.Profile;
+using Fusion.Tests.Core.Auth.Helpers;
+using Fusion.Tests.Core.Profile.Helpers;
+using NSubstitute;
+
+namespace Fusion.Tests.Core.Auth;
+
+public class AuthServiceTests
+{
+    private readonly IAuthService _sut;
+
+    private readonly IManagementApiClient _managementApiClient;
+    private readonly IProfileRepository _profileRepository;
+
+    public AuthServiceTests()
+    {
+        _managementApiClient = Substitute.For<IManagementApiClient>();
+        _profileRepository = Substitute.For<IProfileRepository>();
+
+        _sut = new AuthService(_managementApiClient, _profileRepository);
+    }
+
+    [Fact]
+    public async Task AssignUserToRole_RolesNull_ArgumentNullException()
+    {
+        var faker = new UserRoleDtoFakeProvider();
+        var dto = faker.Get();
+        dto.Roles = null;
+
+        var action = () => _sut.AssignUserToRole(dto, CancellationToken.None);
+        await action.Should().ThrowExactlyAsync<ArgumentNullException>();
+    }
+
+    [Theory]
+    [ClassData(typeof(UserRoleDtoFakeProvider))]
+    public async Task AssignUserToRole_RolesValid_Success(UserRoleDto dto)
+    {
+        var profileFaker = new ProfileDtoFakeProvider();
+        var profileDto = profileFaker.Get();
+
+        var profileEntity = new ProfileEntity
+        {
+            Id = profileDto.Id,
+            FirstName = profileDto.FirstName,
+            LastName = profileDto.LastName,
+            Email = profileDto.Email,
+            Auth0UserId = profileDto.Auth0UserId
+        };
+
+        _profileRepository.GetById(Arg.Any<long>())!
+            .Returns(Task.FromResult(profileEntity));
+
+        var existingRoles = AuthDefaults
+            .Roles.All.Values
+            .ExceptBy(dto.Roles!, x => x.Name)
+            .ToList();
+        var pagedList = new PagedList<Role>(existingRoles.Select(x => new Role
+        {
+            Id = x.Id,
+            Name = x.Name,
+            Description = x.Description
+        }));
+        _managementApiClient.Users.GetRolesAsync(Arg.Any<string>(), cancellationToken: Arg.Any<CancellationToken>())
+            .Returns(Task.FromResult(pagedList as IPagedList<Role>));
+
+        _managementApiClient.Users.AssignRolesAsync(
+                Arg.Any<string>(), Arg.Any<AssignRolesRequest>(), Arg.Any<CancellationToken>())
+            .Returns(Task.CompletedTask);
+
+        var result = await _sut.AssignUserToRole(dto, CancellationToken.None);
+
+        result.Should().NotBeNull();
+        result.IsError.Should().BeFalse();
+        result.Value.FusionUserId.Should().Be(dto.FusionUserId);
+        result.Value.Roles.Should().BeEquivalentTo(AuthDefaults.Roles.All.Keys);
+    }
+
+    [Theory]
+    [ClassData(typeof(UserRoleDtoFakeProvider))]
+    public async Task UnAssignUserToRole_RolesValid_Success(UserRoleDto dto)
+    {
+        var profileFaker = new ProfileDtoFakeProvider();
+        var profileDto = profileFaker.Get();
+
+        var profileEntity = new ProfileEntity
+        {
+            Id = profileDto.Id,
+            FirstName = profileDto.FirstName,
+            LastName = profileDto.LastName,
+            Email = profileDto.Email,
+            Auth0UserId = profileDto.Auth0UserId
+        };
+
+        _profileRepository.GetById(Arg.Any<long>())!
+            .Returns(Task.FromResult(profileEntity));
+
+        var existingRoles = AuthDefaults.Roles.All.Values;
+        var pagedList = new PagedList<Role>(existingRoles.Select(x => new Role
+        {
+            Id = x.Id,
+            Name = x.Name,
+            Description = x.Description
+        }));
+        _managementApiClient.Users.GetRolesAsync(
+                Arg.Any<string>(), cancellationToken: Arg.Any<CancellationToken>())
+            .Returns(Task.FromResult(pagedList as IPagedList<Role>));
+
+        _managementApiClient.Users.RemoveRolesAsync(
+                Arg.Any<string>(), Arg.Any<AssignRolesRequest>(), Arg.Any<CancellationToken>())
+            .Returns(Task.CompletedTask);
+
+        var result = await _sut.UnAssignUserFromRole(dto, CancellationToken.None);
+
+        result.Should().NotBeNull();
+        result.IsError.Should().BeFalse();
+        result.Value.FusionUserId.Should().Be(dto.FusionUserId);
+
+        var expectedRoleIds = AuthDefaults.Roles.All
+            .Where(x => !dto.Roles!.Contains(x.Key))
+            .Select(x => x.Value.Id);
+        result.Value.Roles.Should().BeEquivalentTo(expectedRoleIds);
+    }
+}
---
+++ backend/Fusion/Fusion.Tests/Core/Auth/Helpers/UserRoleDtoFakeProvider.cs
@@ -0,0 +1,13 @@
+﻿using Bogus;
+using Fusion.Core.Auth;
+using Fusion.Tests.Helpers;
+
+namespace Fusion.Tests.Core.Auth.Helpers;
+
+public class UserRoleDtoFakeProvider() : ObjectFakeProviderBase<UserRoleDto>(5)
+{
+    protected override Func<Faker<UserRoleDto>> DefaultFactory =>
+        () => new Faker<UserRoleDto>()
+            .RuleFor(x => x.FusionUserId, f => f.Random.Int())
+            .RuleFor(x => x.Roles, f => f.Random.ArrayElements(AuthDefaults.Roles.All.Keys.ToArray(), 2).ToList());
+}

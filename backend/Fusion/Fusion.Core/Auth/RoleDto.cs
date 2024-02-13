0fbca671d322122875a0d0d05a31173343780f09
Bardin08 <vbardin810@gmail.com>
2024-02-13 23:46:45 +0200

    Exclude DB snapshot from test coverage and update models

---
+++ backend/Fusion/Fusion.Core/Auth/RoleDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Fusion.Core.Auth;
+
+public record RoleDto
+{
+    public required string Id { get; init; }
+    public required string Name { get; init; }
+    public required string Description { get; init; }
+}

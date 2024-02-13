cf6962c6d3b5b2a810f692c9f9b52217c115f53f
Bardin08 <vbardin810@gmail.com>
2024-02-14 00:44:50 +0200

    Remove unused auth errors

--- backend/Fusion/Fusion.Core/Auth/AuthErrors.cs
+++ backend/Fusion/Fusion.Core/Auth/AuthErrors.cs
@@ -4,12 +4,6 @@ namespace Fusion.Core.Auth;
 
 public static class AuthErrors
 {
-    public static ErrorOr<UserRoleDto> ValidationFailed =>
-        Error.Validation("User.Role.ValidationFailed", "Profile DTO validation failed");
-
     public static ErrorOr<UserRoleDto> ProfileMisconfigured =>
         Error.Validation("User.Profile.Misconfigured", "User's profile isn't configured properly");
-
-    public static ErrorOr<UserRoleDto> NotFound(long id) =>
-        Error.NotFound("User.Role.NotFound", $"Profile with ID( {id} ) not found");
 }
